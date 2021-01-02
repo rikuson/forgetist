1da29e2f85bedc267ceab6a4be2d22eee5ef7b19
Rikson <rikuson@users.noreply.github.com>
2021-01-02 12:25:19 +0900

    Update initial scripts

--- postinstall.js
+++ postinstall.js
@@ -1,22 +1,20 @@
 const os = require('os');
 const fs = require('fs');
 const path = require('path');
-const { getInstalledPathSync } = require('get-installed-path');
 const { trash } = require('./lib');
 
 const tmp = path.resolve(os.tmpdir(), 'forgetist.sqlite3');
-const current = path.resolve(getInstalledPathSync('forgetist'), 'trash.sqlite3');
+const current = path.resolve(__dirname, 'trash.sqlite3');
 
 new Promise((resolve, reject) => {
   fs.stat(
-    path.resolve(tmp),
+    tmp,
     (err, stat) => err ? reject(err) : resolve(stat),
   );
 }).then(stat => {
-  fs.copyFileSync(
-    path.resolve(tmp),
-    path.resolve(current),
-  );
-}).catch(console.info);
+  fs.copyFileSync(tmp, current);
+}).catch(() => {
+  trash.init();
+});
 
 trash.migrate();
--- preinstall.js
+++ preinstall.js
@@ -1,19 +1,15 @@
 const os = require('os');
 const fs = require('fs');
 const path = require('path');
-const { getInstalledPathSync } = require('get-installed-path');
 
 const tmp = path.resolve(os.tmpdir(), 'forgetist.sqlite3');
-const current = path.resolve(getInstalledPathSync('forgetist'), 'trash.sqlite3');
+const current = path.resolve(__dirname, 'trash.sqlite3');
 
 new Promise((resolve, reject) => {
   fs.stat(
-    path.resolve(current),
+    current,
     (err, stat) => err ? reject(err) : resolve(stat),
   );
 }).then(stat => {
-  fs.copyFileSync(
-    path.resolve(current),
-    path.resolve(tmp),
-  );
-}).catch(console.info);
+  fs.copyFileSync(current, tmp);
+}).catch(err => {});
